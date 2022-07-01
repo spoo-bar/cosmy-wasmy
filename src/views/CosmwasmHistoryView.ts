ce157bbddc84e2d0252d226ea5a15ddbebfd2537
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2022-07-01 23:55:05 +0530

    cleanup stuff

--- src/views/CosmwasmHistoryView.ts
+++ src/views/CosmwasmHistoryView.ts
@@ -20,69 +20,57 @@ export class CosmwasmHistoryView {
         const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, 'media', 'vscode.css'));
         const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, 'media', 'main.css'));
 
-        let tableContent = "";
-        this.history.forEach((item, i) => {
-            tableContent += "<tr>";
-            tableContent += "<td class=\"line-num\">" + i + "</td>";
-            tableContent += "<td><button id=\"" + i + "\" class=\"tertiary\">▶</button></td>";
-            tableContent += "<td>" + Action[item.actionType] + "</td>";
-            const contract = this.contracts.filter(c => c.contractAddress === item.contractAddr);
-            if (contract && contract.length > 0) {
-                tableContent += "<td>" + contract[0].codeId + ": " + contract[0].label + "</td>";
-            }
-            else {
-                tableContent += "<td><span class=\"error\"><i>Contract address not found in imported contracts.</i></span></td>";
-            }
-            tableContent += "<td>" + item.inputData + "</td>";
-            tableContent += "<td>" + item.contractAddr + "</td>";
-            tableContent += "</tr>";
-        })
-
         webview.onDidReceiveMessage(data => {
             switch (data.type) {
                 case 'exec': {
                     const action = this.history[data.value];
-                    const contract = this.contracts.filter(c => c.contractAddress === action.contractAddr)[0]
-                    switch (action.actionType) {
-                        case Action.Query: {
-                            vscode.window.withProgress({
-                                location: vscode.ProgressLocation.Notification,
-                                title: "Querying the contract - " + contract.label,
-                                cancellable: false
-                            }, (progress, token) => {
-                                token.onCancellationRequested(() => { });
-                                progress.report({ message: '' });
-                                return new Promise(async (resolve, reject) => {
-                                    let data = JSON.parse(action.inputData);
-                                    await Cosmwasm.Query(contract, data, resolve, reject);
+                    const contract = this.getContract(action);
+                    if (contract) {
+                        switch (action.actionType) {
+                            case Action.Query: {
+                                vscode.window.withProgress({
+                                    location: vscode.ProgressLocation.Notification,
+                                    title: "Querying the contract - " + contract.label,
+                                    cancellable: false
+                                }, (progress, token) => {
+                                    token.onCancellationRequested(() => { });
+                                    progress.report({ message: '' });
+                                    return new Promise(async (resolve, reject) => {
+                                        let data = JSON.parse(action.inputData);
+                                        await Cosmwasm.Query(contract, data, resolve, reject);
+                                    });
                                 });
-                            });
-                            break;
-                        }
-                        case Action.Tx: {
-                            const account = Workspace.GetSelectedAccount();
-                            if (!account) {
-                                vscode.window.showErrorMessage("No account selected. Select an account from the Accounts view.");
-                                return;
+                                break;
                             }
-                            vscode.window.withProgress({
-                                location: vscode.ProgressLocation.Notification,
-                                title: "Executing msg on the contract - " + contract.label,
-                                cancellable: false
-                            }, (progress, token) => {
-                                token.onCancellationRequested(() => { });
-                                progress.report({ message: '' });
-                                return new Promise(async (resolve, reject) => {
-                                    let data = JSON.parse(action.inputData);
-                                    await Cosmwasm.Execute(account, contract, data, resolve, reject);
+                            case Action.Tx: {
+                                const account = Workspace.GetSelectedAccount();
+                                if (!account) {
+                                    vscode.window.showErrorMessage("No account selected. Select an account from the Accounts view.");
+                                    return;
+                                }
+                                vscode.window.withProgress({
+                                    location: vscode.ProgressLocation.Notification,
+                                    title: "Executing msg on the contract - " + contract.label,
+                                    cancellable: false
+                                }, (progress, token) => {
+                                    token.onCancellationRequested(() => { });
+                                    progress.report({ message: '' });
+                                    return new Promise(async (resolve, reject) => {
+                                        let data = JSON.parse(action.inputData);
+                                        await Cosmwasm.Execute(account, contract, data, resolve, reject);
+                                    });
                                 });
-                            });
-                            break;
-                        }
-                        default: {
+                                break;
+                            }
+                            default: {
 
+                            }
                         }
                     }
+                    else {
+                        vscode.window.showErrorMessage("Contract not found in imported contracts. Try importing the address again and running the query");
+                    }
+
                 }
             }
         })
@@ -99,19 +87,7 @@ export class CosmwasmHistoryView {
           <title>Cosmwasm History</title>
       </head>
       <body>
-      <div style="overflow-x:auto;">
-      <table>
-        <tr>
-            <th class="line-num">#</th>
-            <th>Run</th>
-            <th>Type</th>
-            <th>Name</th>
-            <th>Input Data</th>
-            <th>Contract Address</th>
-        </tr>
-        ${tableContent}
-      </table>
-      </div>
+      ${this.getViewContent()}
       <script>
       (function () {
           const vscode = acquireVsCodeApi();
@@ -135,6 +111,52 @@ export class CosmwasmHistoryView {
       </body>
       </html>`;
         return
+    }
+
+    private getViewContent() {
+        let content = "";
+        if (Workspace.GetCosmwasmQueriesStored() < 1) {
+            content += `<p>The "cosmywasmy.maxQueryStored" setting is set to zero. Therefore, no query/tx is recorded. Enable that setting to keep history of recorded queries and txs to rerun them easily.</p>`
+        }
+        else if (this.history.length < 1) {
+            content += "The history seems to be empty. Run some queries and txs to record history."
+        } else {
+            content += `<div style="overflow-x:auto;">      
+        <table>
+          <tr>
+              <th class="line-num">#</th>
+              <th>Run</th>
+              <th>Type</th>
+              <th>Label</th>
+              <th>Input Data</th>
+              <th>Contract Address</th>
+          </tr>`;
+            content += this.getTableData();
+            content += "</table></div>"
+        }
+        return content;
+    }
+
+    private getTableData() {
+        let tableContent = "";
+
+        this.history.forEach((item, i) => {
+            tableContent += "<tr>";
+            tableContent += "<td class=\"line-num\">" + i + "</td>";
+            tableContent += "<td><button id=\"" + i + "\" class=\"tertiary\">▶</button></td>";
+            tableContent += "<td>" + Action[item.actionType] + "</td>";
+            const contract = this.getContract(item);
+            if (contract) {
+                tableContent += "<td>" + contract.codeId + ": " + contract.label + "</td>";
+            }
+            else {
+                tableContent += "<td><span class=\"error\"><i>Contract not found in imported contracts.</i></span></td>";
+            }
+            tableContent += "<td>" + item.inputData + "</td>";
+            tableContent += "<td>" + item.contractAddr + "</td>";
+            tableContent += "</tr>";
+        });
+        return tableContent;
     }
 
     private getContract(action: History): Contract | undefined {
