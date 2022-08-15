7894298dc824bf6f75c41831aec25cd419127848
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2022-08-15 23:17:14 +0530

    Create command.ts

---
+++ src/commands/command.ts
@@ -0,0 +1,29 @@
+export class CosmwasmAPI {
+    public static async Register() {
+        // registerAddAccountCmd();
+		// registerRequestFundsCmd();
+		// registerOpenInExplorerCmd();
+		// registerCopyAccountAddressCmd();
+		// registerCopyMnemonicCmd();
+		// registerDeleteAddressCmd();
+		// registerSelectAccountCmd();
+		// registerAddContractCmd();
+		// registerSelectContractCmd();
+		// registerDeleteContractCmd();
+		// registerUpdateContractAdminCmd();
+		// registerClearContractAdminCmd();
+		// registerAddContractCommentCmd();
+		// registerResetDataCmd();
+		// registerReloadConfigCmd();
+		// registerBuildCmd();
+		// registerRunUnitTestsCmd();
+		// registerOptimizeContractCmd();
+		// registerGenerateSchemaCmd();
+		// registerSetUpDevEnvCmd();
+		// registerUploadContractCmd();
+		// registerQueryHistoryCmd();
+		// registerExportDataCmd();
+		// registerQueryCosmwasmCmd();
+		// registerTxCosmwasmCmd();
+    }
+ }
